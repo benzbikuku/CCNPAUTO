7ddba695857ff828f4c7ccdadbb20b0bc96dd7f7
Benz <bikuku.kiyambu@gmail.com>
2024-11-02 05:59:18 +0000

    11/02/2024

---
+++ 23_NXAPI/05_nxapi_cli_show_ascii_demo.py
@@ -0,0 +1,43 @@
+#! /usr/local/Python_envs/Python3/bin/python3
+import re
+
+import requests
+import json
+from pprint import pprint
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+switchuser='admin'
+switchpassword='Kessy,2012'
+
+url='https://192.168.1.96/ins'
+myheaders={'content-type':'application/json'}
+payload={
+  "ins_api": {
+    "version": "1.0",
+    "type": "cli_show_ascii",
+    "chunk": "0",
+    "sid": "sid",
+    "input": "show version",
+    "output_format": "json"
+  }
+}
+# my_pattern = re.compile(r"(?P<ProcessorFamily>\S.+) @ (?P<ProcessorSpeed>\S+) with \d+ \S+ of memory.", flags="m")
+my_pattern = re.compile(r"(?P<ProcessorFamily>\S.+) \n .+ with (?P<Memory>\d+) \S+ of memory.")
+
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword), verify=False).json()
+# print(response)
+cli_ascii_out = response['ins_api']['outputs']['output']['body']
+# print(cli_ascii_out)
+processor_match = my_pattern.search(cli_ascii_out).groupdict()
+print(processor_match)
+print(processor_match['ProcessorFamily'])
+print(processor_match['Memory'])
+
+
+
+
+
+
+
